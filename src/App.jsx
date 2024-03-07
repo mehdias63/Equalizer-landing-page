65439ab9c1da8e9cc1f427d10b66293699e644f8
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-03-07 12:29:44 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import { FaInstagram } from "react-icons/fa";
 
 function App() {
   return (
-    <main className="bg-[url(/images/bg-main-mobile.png)] min-h-screen md:bg-[url(/images/bg-main-tablet.png)] lg:bg-[url(/images/bg-main-desktop.png)] font-ibm relative max-w-[90rem] lg:px-[10rem]">
+    <main className="bg-[url(/images/bg-main-mobile.png)] min-h-screen md:bg-[url(/images/bg-main-tablet.png)] lg:bg-[url(/images/bg-main-desktop.png)] font-ibm relative max-w-[90rem] lg:px-[10rem] mx-auto">
       <img
         src="images/bg-pattern-1.svg"
         className="hidden sm:block absolute sm:right-[-2rem] lg:right-0 max-w-[12.5rem] lg:max-w-[16rem]"
         alt="pattern"
       />
       <div className="p-6">
-        <img src="/images/logo.svg" className="pt-10 lg:pt-6" alt="logo"/>
+        <img src="/images/logo.svg" className="pt-10 lg:pt-6" alt="logo" />
         <div className="text-black">
           <h1 className="text-[2.5rem] md:text-[4rem] lg:text-[5.5rem] font-bold leading-[3rem] md:leading-[4rem] lg:leading-[5.5rem] tracking-[-0.02844rem] md:tracking-[-0.04544rem] lg:tracking-[-0.0625rem] pt-16 pr-20 sm:pr-52 lg:pr-16">
             We make your music sound extraordinary.
@@ -26,7 +26,11 @@ function App() {
         </div>
       </div>
       <div className="bg-black text-white relative flex flex-col items-center rounded-xl sm:h-[37.5rem]">
-        <img src="/images/bg-pattern-2.svg" className="sm:mt-[-4rem]" alt="pattern" />
+        <img
+          src="/images/bg-pattern-2.svg"
+          className="sm:mt-[-4rem]"
+          alt="pattern"
+        />
         <div className="w-[13.06075rem] h-[26.875rem] sm:w-[16.88788rem] sm:h-[34.75rem] absolute top-[-8rem] sm:mr-[20rem] md:mr-[28rem] ">
           <img src="/images/illustration-app.png" alt="illustration" />
         </div>
@@ -58,7 +62,7 @@ function App() {
       <div className="flex flex-col px-5 py-20 sm:flex-row sm:items-center sm:justify-between max-w-[69rem]">
         <div className=" flex flex-col md:flex-row lg:gap-16">
           <div className="pb-8 w-full">
-            <img src="/images/logo.svg" alt="logo"/>
+            <img src="/images/logo.svg" alt="logo" />
           </div>
           <p className="pb-8 text-black text-base leading-[1.625rem] mr-20 max-w-[50%]">
             All rights reserved © Equalizer 2021 Have any problems? Contact us
